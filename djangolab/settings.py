089193a652b484e1358604f631841b27da1ac86a
Rosa Pinccay <rmpincay@espol.edu.ec>
2020-09-28 17:53:44 -0500

    Despliegue Heroku

--- djangolab/settings.py
+++ djangolab/settings.py
@@ -134,6 +134,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+django_heroku.settings(locals())
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
