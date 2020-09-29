281a2f0de3a4daf20291dd9eac56f4b6bdf2981d
Rosa Pinccay <rmpincay@espol.edu.ec>
2020-09-29 16:21:27 -0500

    Configuraciones

--- djangolab/settings.py
+++ djangolab/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'qhgzdkqnyjqtxhf_i2flyuzvu4u0hv#-^*&(7ez1-^98w9(^kp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['amstlabx.herokuapp.com','https://amstlabx.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = ('http://localhost:8000', 'https://amstlabx.herokuapp.com')
