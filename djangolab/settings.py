c20c79ffb5a1e93434908d3fcc4c114810a84f10
Rosa Pinccay <rmpincay@espol.edu.ec>
2020-09-29 16:28:28 -0500

    Update settings.py

--- djangolab/settings.py
+++ djangolab/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'qhgzdkqnyjqtxhf_i2flyuzvu4u0hv#-^*&(7ez1-^98w9(^kp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
