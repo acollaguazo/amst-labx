8d6bcee5bb687549568e3ae18a818c29e0a3d3c1
Rosa Pinccay <rmpincay@espol.edu.ec>
2020-09-29 10:59:39 -0500

    URLs modificadas

--- djangolab/urls.py
+++ djangolab/urls.py
@@ -22,11 +22,11 @@ from rest_framework_jwt.views import refresh_jwt_token
 from rest_framework_jwt.views import verify_jwt_token
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
     url(r'^api/sensores$', views.sensor_data_list),
     url(r'^api/sensores/(?P<pk>[0-9]+)$', views.sensor_data_detail),
-    url(r'^auth-jwt/', obtain_jwt_token),    
-    url(r'^auth-jwt-refresh/', refresh_jwt_token),
-    url(r'^auth-jwt-verify/', verify_jwt_token),
+    url(r'^db/nuevo-jwt', obtain_jwt_token),    
+    url(r'^auth-jwt-refresh', refresh_jwt_token),
+    url(r'^auth-jwt-verify', verify_jwt_token),
 ]
 
