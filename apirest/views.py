467cdd8cf1da028c0cb227deded9addf18d02547
Rosa Pinccay <rmpincay@espol.edu.ec>
2020-09-30 03:25:09 -0500

    views.py modificado

--- apirest/views.py
+++ apirest/views.py
@@ -7,40 +7,45 @@ from apirest.serializers import SensoresSerializer
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 
-@api_view(['GET', 'POST'])
+
+@api_view(['GET', 'POST', 'DELETE'])
 @permission_classes([AllowAny])
 def sensor_data_list(request):
-  if request.method == 'GET':
-    sensor_data = Sensores.objects.all()
-    sensor_data_serializer = SensoresSerializer(sensor_data, many=True)
-    return JsonResponse(sensor_data_serializer.data, safe=False)
-  elif request.method == 'POST':
-    sensor_data = JSONParser().parse(request)
-    sensor_data_serializer = SensoresSerializer(data=sensor_data)
-    if sensor_data_serializer.is_valid():
-      sensor_data_serializer.save()
-      return JsonResponse(sensor_data_serializer.data, status=status.HTTP_201_CREATED) 
-    return JsonResponse(sensor_data_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
- 
- 
+    if request.method == 'GET':
+        sensor_data = Sensores.objects.all()
+        sensor_data_serializer = SensoresSerializer(sensor_data, many=True)
+        return JsonResponse(sensor_data_serializer.data, safe=False)
+    elif request.method == 'POST':
+        sensor_data_data = JSONParser().parse(request)
+        sensor_data_serializer = SensoresSerializer(data=sensor_data_data)
+        if sensor_data_serializer.is_valid():
+            sensor_data_serializer.save()
+            return JsonResponse(sensor_data_serializer.data, status=status.HTTP_201_CREATED) 
+        return JsonResponse(sensor_data_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        count = Sensores.objects.all().delete()
+        return JsonResponse({'message': 
+        '{} sensor data were deleted successfully!'.format(count[0])}, status=status.HTTP_204_NO_CONTENT)
+
+
 @api_view(['GET', 'PUT', 'DELETE'])
 @permission_classes([AllowAny])
 def sensor_data_detail(request, pk):
-  try: 
-    sensor_data = Sensores.objects.get(pk=pk) 
-    if request.method == 'GET': 
-      sensor_data_serializer = SensoresSerializer(sensor_data) 
-      return JsonResponse(sensor_data_serializer.data) 
-    elif request.method == 'PUT': 
-      sensor_data = JSONParser().parse(request) 
-      sensor_data_serializer = SensoresSerializer(sensor_data, data=sensor_data) 
-      if sensor_data_serializer.is_valid(): 
-        sensor_data_serializer.save() 
-        return JsonResponse(sensor_data_serializer.data) 
-      return JsonResponse(sensor_data_serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
-    elif request.method == 'DELETE': 
-      sensor_data.delete() 
-      return JsonResponse({'message': 'sensor data was deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
-  except Sensores.DoesNotExist: 
-    return JsonResponse({'message': 'The sensor data does not exist'}, status=status.HTTP_404_NOT_FOUND) 
-
+    try: 
+        sensor_data = Sensores.objects.get(pk=pk) 
+        if request.method == 'GET': 
+            sensor_data_serializer = SensoresSerializer(sensor_data) 
+            return JsonResponse(sensor_data_serializer.data) 
+        elif request.method == 'PUT': 
+            sensor_data_data = JSONParser().parse(request) 
+            sensor_data_serializer = SensoresSerializer(sensor_data, data=sensor_data_data) 
+            if sensor_data_serializer.is_valid(): 
+                sensor_data_serializer.save() 
+                return JsonResponse(sensor_data_serializer.data) 
+            return JsonResponse(sensor_data_serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+        elif request.method == 'DELETE': 
+            sensor_data.delete() 
+            return JsonResponse({'message': 'sensor data was deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
+    except Sensores.DoesNotExist: 
+        return JsonResponse({'message': 'The sensor data does not exist'}, status=status.HTTP_404_NOT_FOUND) 
+        
